34e3f126a92d53f2b114591368c06395553006c2
Peter Chu <peterc0523@gmail.com>
2023-02-12 12:26:18 -0500

    add day 2

---
+++ day02/solution1.js
@@ -0,0 +1,59 @@
+const { input } = require('./input1');
+
+const solution = (input) => {
+
+  const letter = {
+    A: 'rock',
+    B: 'paper',
+    C: 'scissors',
+    X: 'rock',
+    Y: 'paper',
+    Z: 'scissors',
+  }
+
+  const shape = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+  }
+
+  const outcome = {
+    win: 6,
+    draw: 3,
+    lose: 0
+  }
+
+  let total = 0;
+
+  const isWinner = (arr) => {
+    switch (`${letter[arr[0]]}-${letter[arr[1]]}`) {
+      case 'rock-paper':
+        return 'win';
+      case 'paper-rock':
+        return 'lose';
+      case 'scissors-rock':
+        return 'win';
+      case 'paper-scissors':
+        return 'win';
+      case 'scissors-paper':
+        return 'lose';
+      case 'rock-scissors':
+        return 'lose';
+      case 'rock-rock':
+        return 'draw';
+      case 'paper-paper':
+        return 'draw';
+      case 'scissors-scissors':
+        return 'draw';
+    }
+  }
+
+  input.forEach((e) => {
+    const thisOutcome = isWinner(e);
+    total += outcome[thisOutcome] + shape[letter[e[1]]]
+  })
+
+  return total;
+};
+
+console.log(solution(input));
---
+++ day02/solution2.js
@@ -0,0 +1,54 @@
+const { input } = require('./input2');
+
+const solution = (input) => {
+  const letter = {
+    A: 'rock',
+    B: 'paper',
+    C: 'scissors',
+    X: 'lose',
+    Y: 'draw',
+    Z: 'win',
+  }
+
+  const shape = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+  }
+
+  const outcome = {
+    win: 6,
+    draw: 3,
+    lose: 0
+  }
+
+  let total = 0;
+
+  const myShape = (arr) => {
+    switch (`${letter[arr[0]]}-${letter[arr[1]]}`) {
+      case 'rock-win':
+        return 'paper';
+      case 'rock-lose':
+        return 'scissors';
+      case 'paper-win':
+        return 'scissors';
+      case 'paper-lose':
+        return 'rock';
+      case 'scissors-win':
+        return 'rock';
+      case 'scissors-lose':
+        return 'paper';
+      case `${letter[arr[0]]}-draw`:
+        return letter[arr[0]];
+    }
+  }
+
+  input.forEach((e) => {
+    const thisShape = myShape(e);
+    total += outcome[letter[e[1]]] + shape[thisShape];
+  })
+
+  return total;
+}
+
+console.log(solution(input));
