7ba285f9085ac3433e603c85201e531ddbb9c853
Peter Chu <peterc0523@gmail.com>
2023-02-12 13:14:42 -0500

    add day3 part2 solution

--- day03/part2/solution.js
+++ day03/part2/solution.js
@@ -1,3 +1,27 @@
 const { input } = require('./input');
 
-console.log(input.length);
+const solution = (input) => {
+
+  let total = 0;
+  
+  for (let i = 0; i < input.length; i += 3) {
+    let common;
+
+    for (let j = 0; j < input[i].length; j++) {
+      thisLetter = input[i][j]
+      if (input[i + 1].includes(thisLetter) && input[i + 2].includes(thisLetter)) {
+        common = thisLetter.charCodeAt(0);
+      }
+    }
+
+    if (common <= 90) {
+      total += common - 38
+    } else {
+      total += common - 96
+    }
+  }
+
+  return total;
+}
+
+console.log(solution(input));
